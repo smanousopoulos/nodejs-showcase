fcf1e29776ddefc64f4be0d90840644efdc37479
Stelios Manousopoulos <smanousopoulos@gmail.com>
2017-09-19 02:15:57 +0300

    handle gplaces results in multiple pages

--- api/gplaces.js
+++ api/gplaces.js
@@ -2,14 +2,19 @@ const fetch = require('isomorphic-fetch');
 
 const key = require('./gplaces.key'); 
 
-const geosearch = (lat, lon, radius = 1000, token) => fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=${radius}&type=point_of_interest&key=${key}`)
+const geosearch = (lat, lon, radius = 1000, token = null) => {
+  const baseuri = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${key}`;
+  const uri = token ? `${baseuri}&pagetoken=${token}` : `${baseuri}&location=${lat},${lon}&radius=${radius}&type=point_of_interest`;
+
+  return fetch(uri)
 .then(res => res.json())
 .then((res) => {
-  if (!res.status === 'OK') { 
+  if (res.status !== 'OK') { 
     throw res.status;
   }
   return res;
 });
+}
 
 const transformPOI = poi => ({
     title: poi.name,
--- streams/gplaces.js
+++ streams/gplaces.js
@@ -8,44 +8,56 @@ const GPlacesStream = function(lat, lon, radius = 1000) {
   this.lat = lat;
   this.lon = lon;
   this.radius = radius;
-  this.page = 0;
-  this.started = false;
-  this.hasMore = true;
   this.token = null;
-};
-
+  this.runLock = false;
+  this.hasMore = false;
+}
 util.inherits(GPlacesStream, Readable);
 
+// need to delay execution before asking for next page
+GPlacesStream.prototype.fetch = (lat, lon, radius, token) => new Promise((res, rej) => { 
+  setTimeout(() => {
+    return res(geosearch(lat, lon, radius, token));
+  }, token ? 2000 : 1);
+});
+
+
 GPlacesStream.prototype._read = function(n) {
-  const self = this;
-  if (!this.started) {
-    this.started = true;
-      geosearch(this.lat, this.lon, this.radius, this.token)
-      .then((res) => {
+  if (!this.runLock) {
+    this.runLock = true;
+    
+    this.fetch(this.lat, this.lon, this.radius, this.token)
+    .then((res) => {
+      if (res && res.status === 'OK') {
         if (res.next_page_token) {
-          self.token = res.next_page_token;
+          this.hasMore = true;
+          this.token = res.next_page_token;
         } else {
-          self.hasMore = false;
+          this.hasMore = false;
+          this.token = null;
         }
         return res.results.map(r => transformPOI(r));;
-      })
-      .then((pois) => {
-        self.pages = pois.length;
-        pois.forEach((poi, i) => {
-            this.push(JSON.stringify(poi));  
-            //if (!self.hasMore && (self.page === self.pages - 1)) {
-            if (self.page === self.pages - 1) {
-              this.push(null);
-            }
-            self.page++;
-        });
-      })
-      .catch((err) => {
-        console.error('geosearch error', err);
-        self.hasMore = false;
-        self.push(null);
+      } 
+    })
+    .then((pois) => {
+      pois.forEach((poi, i) => {
+        this.push(JSON.stringify(poi));  
+
+        //unlock when 1 before last
+        if (i === pois.length - 2 && this.hasMore) {
+          this.runLock = false;
+        }
+           
+        if (i === pois.length - 1 && !this.hasMore) {
+          this.push(null);
+        }
       });
-    } 
+    })
+    .catch((err) => {
+      console.error('GPlacesStream error:', err);
+      this.push(null);
+    });
+  }
 };
 
 module.exports = GPlacesStream;
